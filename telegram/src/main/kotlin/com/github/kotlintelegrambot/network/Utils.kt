de668305443309b672085d0e6565a551281b3eef
Joshua Storch <mail@jstorch.net>
2021-09-10 20:54:45 +0200

    feat: extension functions for request errors

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/network/Utils.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/network/Utils.kt
@@ -1,5 +1,7 @@
 package com.github.kotlintelegrambot.network
 
+import com.github.kotlintelegrambot.network.Response as TelegramResponse
+import com.github.kotlintelegrambot.network.serialization.GsonFactory
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
@@ -15,6 +17,33 @@ class ResponseError(val errorBody: ResponseBody?, val exception: Exception?)
 fun <T> Pair<Response<T?>?, Exception?>.fold(response: (T?) -> Unit = {}, error: (ResponseError) -> Unit = {}) {
     if (first?.isSuccessful == true && first?.body() != null) response(first!!.body()!!)
     else error(ResponseError(first?.errorBody(), second))
+}
+
+/**
+ * call the error function [errorFun] if an exception occurred during the request
+ */
+fun <T> Pair<Response<TelegramResponse<T>?>?, Exception?>.onException(
+    errorFun: (e: Exception) -> Unit
+): Pair<Response<TelegramResponse<T>?>?, Exception?> {
+    if (second != null)  {
+        errorFun(second!!)
+    }
+    return this
+}
+
+/**
+ * call the error function [errorFun] if an API HTTP error occurred during the request
+ */
+fun <T> Pair<Response<TelegramResponse<T>?>?, Exception?>.onHttpError(
+    errorFun: (errorResponse: TelegramResponse<Nothing>) -> Unit
+): Pair<Response<TelegramResponse<T>?>?, Exception?> {
+    if (first != null && !first!!.isSuccessful) {
+        errorFun(GsonFactory.createForApiClient().fromJson(
+            first!!.errorBody()!!.string(),
+            com.github.kotlintelegrambot.network.Response::class.java
+        ) as com.github.kotlintelegrambot.network.Response<Nothing>)
+    }
+    return this
 }
 
 fun <T, R> Pair<Response<T?>?, Exception?>.bimap(mapResponse: (T?) -> R, mapError: (ResponseError) -> R): R =
